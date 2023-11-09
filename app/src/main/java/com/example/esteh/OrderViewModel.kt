79d300d920203a0db77aaa745f0a5db77f3eed13
nabielmakareem <nabielmakareem10@gmail.com>
2023-11-09 11:45:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/esteh/OrderViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.esteh
+
+import androidx.lifecycle.ViewModel
+import com.example.esteh.data.OrderUIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import java.text.NumberFormat
+
+private const val HARGA_PER_CUP = 3000
+class OrderViewModel : ViewModel(){
+    private val _stateUI = MutableStateFlow(OrderUIState())
+    val stateUI:StateFlow<OrderUIState> = _stateUI.asStateFlow()
+
+    fun setJumalah(jmlesteh:Int){
+        _stateUI.update { stateSaatIni ->
+            stateSaatIni.copy(
+                jumlah = jmlesteh,
+                harga = hitungHarga(jumlah =jmlesteh)
+            )
+        }
+    }
+    fun setRasa(rasaPilihan: String){
+       _stateUI.update { stateSaatIni ->
+           stateSaatIni.copy(rasa = rasaPilihan)
+       }
+    }
+    fun resetOrder(){
+        _stateUI.value = OrderUIState()
+    }
+    private fun hitungHarga(
+        jumlah : Int = _stateUI.value.jumlah
+    ): String{
+        val kalkulasiHarga = jumlah * HARGA_PER_CUP
+
+        return NumberFormat.getNumberInstance().format(kalkulasiHarga)
+    }
+}
---
+++ app/src/main/java/com/example/esteh/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.esteh.data
+
+import com.example.esteh.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche
+    )
+}
